1d0a6dce0900095a4fc3047cbe2df2e1e3bc2a3c
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-08-09 18:58:21 -0300

    se agrega la logica para que los gifs se muestren inicialmente en la categoria random y seugn se vaya seleccionando, cambien a la categoria asignada

--- src/components/Gimoji.jsx
+++ src/components/Gimoji.jsx
@@ -11,17 +11,34 @@ const urlApi = import.meta.env.VITE_URL_API;
 
 const Gimoji = () => {
   const [categories, setCategories] = useState([])
+  const [gifs, setGifs] = useState([])
+  const [search, setSearch] = useState("random")
 
   useEffect(() => {
     getCategories();
+    getInit();
   }, [])
 
+  useEffect(() =>{
+    getInit()
+  }, [search])
+  
   const getCategories = async () => {
     const respuesta = await fetch(`${urlApi}gifs/categories?api_key=${apiKey}`)
     const { data } = await respuesta.json()
     setCategories(data)
   }
 
+  const getInit = async () => {
+    const respuesta = await fetch(
+      `${urlApi}gifs/search?api_key=${apiKey}&q=${search}&limit=24&offset=08`)
+    const { data } = await respuesta.json()
+    setGifs(data)
+  }
+
+  const onChangeByCategory = (event) => {
+    setSearch(event.target.value)
+  }
     return (
     <>
       <Navegacion></Navegacion>
@@ -29,14 +46,17 @@ const Gimoji = () => {
       <div className="seccionPrincipal">
       <div className="mb-1">
         <Row className="justify-content-center">
-          <SelectorCategorias    categories={categories}     />
+          <SelectorCategorias    
+          categories={categories} 
+          onChangeByCategory={(event) =>onChangeByCategory(event)}    />
           <Buscador/>
         </Row>
       </div>
       <div className="py-2 mt-5">
             <div className="container">
                 <Row className="">
-                   <GifCard></GifCard>
+                   <GifCard 
+                   gifs={gifs}></GifCard>
                 </Row>
             </div>
         </div>   
--- src/components/ui/SelectorCategorias.jsx
+++ src/components/ui/SelectorCategorias.jsx
@@ -1,11 +1,12 @@
 import { Col } from "react-bootstrap";
 
-const SelectorCategorias = ({categories}) => {
+const SelectorCategorias = ({categories, onChangeByCategory}) => {
     return(
         <Col md={4} lg={2}>
         <select 
         className="form-select" 
         aria-label="Default select example" 
+        onChange={(event) => onChangeByCategory(event)}
         >
         <option defaultValue={true}>Selecciona una Categoria</option>
         {categories.map( (categ) => (
