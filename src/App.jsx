57cc6c2cbbcedcea29afb75cd20add33b45e9b9c
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-08-09 09:47:02 -0300

    maquetado navbar realizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Gimoji from './components/Gimoji';
 
 function App() {
 
   return (
     <>
+    <Gimoji/>
     </>
   )
 }
---
+++ src/components/Gimoji.jsx
@@ -0,0 +1,11 @@
+import Navegacion from "./header/Navegacion"
+
+const Gimoji = () => {
+    return (
+        <>
+        <Navegacion></Navegacion>
+        </>
+    )
+}
+
+export default Gimoji
