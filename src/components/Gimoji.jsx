dc99b0c159159783ff991f25142bc9ab583f5201
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-08-09 10:29:11 -0300

    maquetado gif cards

--- src/components/Gimoji.jsx
+++ src/components/Gimoji.jsx
@@ -3,18 +3,28 @@ import Banner from "./header/Banner";
 import Navegacion from "./header/Navegacion";
 import SelectorCategorias from "./ui/SelectorCategorias";
 import Buscador from "./ui/Buscador";
+import GifCard from "./GifCard";
 
 const Gimoji = () => {
   return (
     <>
       <Navegacion></Navegacion>
       <Banner />
-      <div className="seccionPrincipal mb-1">
+      <div className="seccionPrincipal">
+      <div className="mb-1">
         <Row className="justify-content-center">
           <SelectorCategorias         />
           <Buscador/>
         </Row>
       </div>
+      <div className="py-2 mt-5">
+            <div className="container">
+                <Row className="">
+                   <GifCard></GifCard>
+                </Row>
+            </div>
+        </div>   
+      </div>                 
     </>
   );
 };
--- src/components/ui/SelectorCategorias.jsx
+++ src/components/ui/SelectorCategorias.jsx
@@ -2,12 +2,12 @@ import { Col } from "react-bootstrap";
 
 const SelectorCategorias = () => {
     return(
-        <Col md={4} lg={3}>
+        <Col md={4} lg={2}>
         <select 
         className="form-select" 
         aria-label="Default select example" 
         >
-            <option defaultValue={true}>Selecciona una Categoria</option>
+        <option defaultValue={true}>Selecciona una Categoria</option>
         </select>
   </Col>
     )
